ef7fbea745efafa7e0cf091462e37e8afba76643
Arka Mondal <arkamondal1995@outlook.com>
2020-03-23 11:38:07 +0530

    3rd Commit 22/03/2020

--- app/src/main/java/com/example/logindemo/HomeFragment.kt
+++ app/src/main/java/com/example/logindemo/HomeFragment.kt
@@ -8,12 +8,18 @@ import android.view.ViewGroup
 
 class HomeFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+
     }
 
 }
